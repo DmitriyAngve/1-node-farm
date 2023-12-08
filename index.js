31fa0c391f250ecc120f9f902abe2c51bc2aca5e
DmitriyAngve <kirirch129@gmail.com>
2023-12-08 18:49:27 +0300

    #

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs"); // использую этот модуль для получения доступа к чтению и написанию данных в файловой системе. Возвращает объект с множетсвом функций, которые используются в дальнейшем
+
+// Синхронная версия функции для чтения файлов. Принимает два аргумента. Первый с путём до файла, который необходимо прочитать и Второй кодировку этого файла
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+// Теперь запишем файл
+const textOut = `This is waht we know about avokado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written!");
